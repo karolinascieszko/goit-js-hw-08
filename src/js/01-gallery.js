34a4e4ae1bd20aac487b7aea1eab886cd9041464
Karolina Ścieszko <karolinascieszko@gmail.com>
2021-09-06 13:14:38 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,5 @@
-'use strict';
-import _default from '../../node_modules/simplelightbox/dist/simple-lightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(galleryItems);
-const qs = selector => document.querySelector(selector);
-const qsa = selector => document.querySelectorAll(selector);
-//ADD GALLERY ITEMS
-const galleryBox = qs('.gallery');
-console.log(galleryBox);
-const images = galleryItems
-  .map(
-    item =>
-      `<a class = "gallery__item" href="${item.original}">
-            <img class="gallery__image" src="${item.preview}"  alt="${item.description}" title="${item.description}"/> 
-            </a>`,
-  )
-  .join('');
 
-galleryBox.insertAdjacentHTML('afterbegin', images);
-console.log(images);
-//ADDITIONAL OPTIONS
-let lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
-console.log(lightbox);
+console.log(galleryItems);
